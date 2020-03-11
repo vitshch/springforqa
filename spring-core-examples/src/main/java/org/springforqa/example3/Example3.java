a15d34f0a88c1ccbfb185bbdce3c03194192b284
Vitalii Shcherban <vitali.shcherban@gmail.com>
2020-03-11 09:40:48 +0100

    Change to AbstractApplicationContext

--- spring-core-examples/src/main/java/org/springforqa/example3/Example3.java
+++ spring-core-examples/src/main/java/org/springforqa/example3/Example3.java
@@ -1,6 +1,6 @@
 package org.springforqa.example3;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 /**
  * Example3 shows How to create Annotation based config
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Example3 {
 
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppContext.class);
+        AbstractApplicationContext context = new AnnotationConfigApplicationContext(AppContext.class);
         Bean1 bean1 = context.getBean("bean1", Bean1.class);
         System.out.println(bean1.getMessage());
     }
